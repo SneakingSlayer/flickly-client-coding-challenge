a3f18dae6794b83f748a417facbe18ce32162d75
SneakingSlayer <endayalance@yahoo.com>
2025-01-03 07:22:06 +0800

    hotfix: increase timeout for server cold start

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create axios instance for API
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    timeout: 5000,
+    timeout: 60000, // 60 seconds
 });
 
 export default axiosInstance;
